8cc3b8b8a51b006b21ad6ccbf7fcbba4c9f9aaac
Vindhywasini Gupta <gvindhywasini@gmail.com>
2025-06-20 14:02:19 +0530

    kk

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Enable static HTML export (drops files into "out/")
+  output: 'export',
 };
 
 export default nextConfig;
+
